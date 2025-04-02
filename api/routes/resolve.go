012fed3d33a563464d8dd9bdc155aae1d4a53145
Oyedele_Musa_Funso <56215827+mfoyedele@users.noreply.github.com>
2025-04-02 08:52:54 +0100

    Error handling implemented in resolve.go

--- api/routes/resolve.go
+++ api/routes/resolve.go
@@ -1,9 +1,9 @@
 package routes
 
 import (
-	"github.com/mfoyedele/shorten-url-fiber-redis/database"
 	"github.com/go-redis/redis/v8"
 	"github.com/gofiber/fiber/v2"
+	"github.com/mfoyedele/shorten-url-fiber-redis/database"
 )
 
 func ResolveURL(c *fiber.Ctx) error {
@@ -13,9 +13,20 @@ func ResolveURL(c *fiber.Ctx) error {
 	defer r.Close()
 
 	value, err := r.Get(database.Ctx, url).Result()
-	if err == redis.Nil{
+	if err == redis.Nil {
 		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
-			"error": "short not found in the database"
+			"error": "short not found in the database",
+		})
+	} else if err != nil {
+		return c.Status(fiber.StatusInternalError).JSON(fiber.Map{
+			"error": "cannot connect to DB",
 		})
 	}
+
+	rInr := database.CreateClient(1)
+	defer rInr.Close()
+
+	_ = rInr.Incr(database.Ctx, "counter")
+
+	return c.Redirect(value, 301)
 }
