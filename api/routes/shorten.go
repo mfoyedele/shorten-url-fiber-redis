38d003284cfe67b6befecb1c2c1eced68d6f313e
Oyedele_Musa_Funso <56215827+mfoyedele@users.noreply.github.com>
2025-04-05 04:18:29 +0100

    Update shorten.go

--- api/routes/shorten.go
+++ api/routes/shorten.go
@@ -106,7 +106,20 @@ func ShortenURL(c *fiber.Ctx) error {
 		})
 	}
 
+	// respond with the url, short, expiry in hours, calls remaining and time to reset
+	resp := response{
+		URL:             body.URL,
+		CustomShort:     "",
+		Expiry:          body.Expiry,
+		XRateRemaining:  10,
+		XRateLimitReset: 30,
+	}
 	r2.Decr(database.Ctx, c.IP())
+	val, _ = r2.Get(database.Ctx, c.IP()).Result()
+	resp.XRateRemaining, _ = strconv.Atoi(val)
+	ttl, _ := r2.TTL(database.Ctx, c.IP()).Result()
+	resp.XRateLimitReset = ttl / time.Nanosecond / time.Minute
 
-	return c.Status(fiber.StatusOK).JSON(err)
+	resp.CustomShort = os.Getenv("DOMAIN") + "/" + id
+	return c.Status(fiber.StatusOK).JSON(resp)
 }
